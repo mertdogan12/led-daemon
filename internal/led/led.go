48f2b7f52b5bf92aa1363fce41e7773542bae5e4
Mert Dogan <mert.dogan@me.com>
2022-12-03 01:05:32 +0100

    leg changed

--- internal/led/led.go
+++ internal/led/led.go
@@ -1,11 +1,17 @@
 package led
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"time"
 
 	"github.com/mertdogan12/led-daemon/internal/uds"
 )
+
+var DeviceCount int = 1
+var subnet string = "192.168.100."
+var port string = "1337"
 
 func Run() {
 	for {
@@ -16,9 +22,28 @@ func Run() {
 			log.Printf("Mode %s does not exist. Mode set to default (off).", uds.Mode)
 			uds.Mode = "off"
 		}
+
+		time.Sleep(100 * time.Millisecond)
 	}
 }
 
 func off() {
-	time.Sleep(5 * time.Second)
+	for i := 2; i < 2+DeviceCount; i++ {
+		addr := fmt.Sprintf("%s%d:%s", subnet, i, port)
+		conn, err := net.Dial("udp", addr)
+
+		if err != nil {
+			log.Println("Could not open connection to:", addr)
+			continue
+		}
+
+		_, err = conn.Write([]byte{0x00, 0x00, 0x00, 0x00})
+		if err != nil {
+			log.Println("Could not send a udp package to:", addr)
+			conn.Close()
+			continue
+		}
+
+		conn.Close()
+	}
 }
