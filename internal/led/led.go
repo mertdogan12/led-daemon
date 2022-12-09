5860c31ece1534aab2719afbcd8656dd8e56a219
Mert Dogan <mert.dogan@me.com>
2022-12-09 22:57:44 +0100

    works

--- internal/led/led.go
+++ internal/led/led.go
@@ -1,6 +1,7 @@
 package led
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
@@ -18,6 +19,8 @@ func Run() {
 		switch uds.Mode {
 		case "off":
 			off()
+		case "color":
+			color()
 		default:
 			log.Printf("Mode %s does not exist. Mode set to default (off).", uds.Mode)
 			uds.Mode = "off"
@@ -28,6 +31,40 @@ func Run() {
 }
 
 func off() {
+	setColor(0, 0, 0)
+
+	time.Sleep(time.Second)
+}
+
+func color() {
+	setColor(0, 0, 255)
+
+	time.Sleep(time.Second)
+}
+
+func setColor(red uint16, green uint16, blue uint16) {
+	multi := uint16(1024 / 255)
+
+	data := make([]byte, 0)
+	tmp := make([]byte, 2)
+
+	binary.LittleEndian.PutUint16(tmp, 1024-red*multi) // Red
+	data = append(data, tmp...)
+
+	binary.LittleEndian.PutUint16(tmp, 1024-green*multi) // Green
+	data = append(data, tmp...)
+
+	binary.LittleEndian.PutUint16(tmp, 1024-blue*multi) // Blue
+	data = append(data, tmp...)
+
+	sendData(data)
+}
+
+func sendData(data []byte) {
+	if len(data) > 6 {
+		log.Fatal("Send data is to big")
+	}
+
 	for i := 2; i < 2+DeviceCount; i++ {
 		addr := fmt.Sprintf("%s%d:%s", subnet, i, port)
 		conn, err := net.Dial("udp", addr)
@@ -37,7 +74,7 @@ func off() {
 			continue
 		}
 
-		_, err = conn.Write([]byte{0x00, 0x00, 0x00, 0x00})
+		_, err = conn.Write(data)
 		if err != nil {
 			log.Println("Could not send a udp package to:", addr)
 			conn.Close()
@@ -46,4 +83,5 @@ func off() {
 
 		conn.Close()
 	}
+
 }
